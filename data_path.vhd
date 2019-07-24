fe049ed858f7a899c371780b0fd68e1683dd0995
Yorguin Jose Mantilla <yjmantilla@gmail.com>
2019-07-23 19:11:36 -0500

    multiplier works!

---
+++ data_path.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:09:22 07/17/2019 
+-- Design Name: 
+-- Module Name:    data_path - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity data_path is
+end data_path;
+
+architecture Behavioral of data_path is
+
+begin
+
+
+end Behavioral;
+
